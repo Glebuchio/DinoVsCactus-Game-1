08c690bb5533439ef2a308ccfc0d1d89076416c0
Glebuchio <fronlondon@gmail.com>
2024-09-01 14:56:21 +0500

    finished project

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-const dino = document.getElementById('dino'); //ищем в html элемент с id dino
-const cactus = document.getElementById('cactus');
-function jump(){
-    if (dino.classList !='jump') {
-        dino.classList.add('jump');
 
-    setTimeout (function () {
-        dino.classList.remove('jump');
-    }, 300)
-  }
-}
+    // Ищем в html элемент с id dino
+const dino = document.getElementById('dino'); 
 
-let isAlive = setInterval(function () {
+    // Ищем в html элемент с id cactus
+const cactus = document.getElementById('cactus'); 
+
+        // Функция jump() отвечает за выполнение действия прыжка для элемента с dino
+    function jump(){
+        if (dino.classList !='jump') {
+            dino.classList.add('jump');
+
+            setTimeout (function () {
+                dino.classList.remove('jump');
+            }, 300)
+        }
+    }
+
+        // Делаем игру интерактивной 
+        // Выполняем проверку состояния игры для проверки столкновения между динозавром и кактусом 
+        // Если кактус слишком близко и динозавр не в прыжке, игроку показывается предупреждение "Uh-oh... Try again"
+    let isAlive = setInterval(function () {
 
 
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
     
     
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'))
-    console.log(cactusLeft)
+            let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'))
+                console.log(cactusLeft)
 
- if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-    alert('Uh-oh... Try again')
- }
+                if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+                    alert('Uh-oh... Try again')
+                }
 
-}, 10)
+    }, 10)
 
+    // Добавляем обработчик событий к документу, чтобы слушать нажатия клавиш
+    // Код позволяет динозавру прыгать когда пользователь нажимает любую клавишу на клавиатуре
 document.addEventListener('keydown', function(event) {
     jump()
 });
